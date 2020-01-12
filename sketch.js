88acac03f3b08fdce482fe2d590d4893e01a1dfb
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2020-01-12 20:09:47 +0100

    first upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,72 @@
-function preload(){
-  // put preload code here
-}
+var speaker;
+var counter = 0;
+var party_music;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight, WEBGL)
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  party_music = loadSound('assets/party-music.mp3')
+  speaker = loadModel("assets/Speaker.obj", true); // If true, scale the model to a standardized size when loading (normalization)
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background('black');
+  background(0);
+  counter++;
+  // camera(width/2, height/2, 0, 0, 0, 0, 0, 1, 0);
+
+  // normalMaterial(255);
+specularMaterial(50,50,50);
+
+
+  //give a basic color
+  //directionalLight(v1,v2,v3,position), the position need units between 0 to 1
+
+if(counter>=100000 && counter<100008){
+  ambientLight(30,30,70);
+  directionalLight(255, 0, 255, 0.5, 1, 0);
+  pointLight(0,255,255, height / 2, width / 2, 0);
+}
+
+else if(counter>=100008 && counter<100016){
+  ambientLight(70,30,50);
+  directionalLight(255, 0, 10, 1, 0.5, 0);
+  pointLight(0,255,0, height / 2, width / 2, 0);
+}
+
+else if(counter>=100016 && counter<100024){
+  ambientLight(30,70,30);
+  directionalLight(0, 255, 100, 0.8, 0.2, 0);
+  pointLight(255,0,0, height / 2, width / 2, 0);
+}
+
+else if (counter == 100024){
+  counter=100000;
+}
+
+if (counter<100008){
+  ambientLight(70,70,70);
+  directionalLight(70, 70,70, 0.8, 0.2, 0);
+  pointLight(100, 100,100, height / 2, width / 2, 0);
+}
+
+
   noStroke();
+  box(width,height,10);
+  rotateZ(180);
+  rotateY((-mouseX*0.05)+45);
+  rotateX((mouseY*0.05)-10);
 
-  push();
-  translate(0,0,500);
-  ambientLight(50,50,50);
-  //directionalLight(v1,v2,v3,position), the position need units between 0 to 1
-  directionalLight(255,255, 50, 0.5,1, -1);
-  pointLight(255,255,50, 0,0, 0);
-  box(200, 200);
-  pop();
+  model(speaker);
+}
 
-  push();
-  translate(0,0,500);
-  ambientLight(50,50,50);
-  //directionalLight(v1,v2,v3,position), the position need units between 0 to 1
-  directionalLight(255,50, 50, 0.5,1, -1);
-  sphere(20,20);
-  pop();
+function mouseClicked() {
+  if (party_music.isPlaying()) {
+    party_music.pause();
+    counter = 0;
+  } else {
+    party_music.play();
+    counter = 100000;
 
 
-  // put drawing code here
+  }
 }
